acbcb6f4efbcc86e8d2e8c9eebab739702260301
Alex Bujenita <alex.bujenita@sainsburys.co.uk>
2026-04-05 14:44:16 +0100

    docker_compose volumes and app connection

--- src/main/java/org/truje/jflix/JFlix.java
+++ src/main/java/org/truje/jflix/JFlix.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JflixApplication {
+public class JFlix {
 
     public static void main(String[] args) {
-        SpringApplication.run(JflixApplication.class, args);
+        SpringApplication.run(JFlix.class, args);
     }
 }
