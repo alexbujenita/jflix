c056dbd85c3474e127073c761739e9bae1609ccb
Alex Bujenita <alex.bujenita@sainsburys.co.uk>
2026-07-06 21:27:50 +0100

    Some updates.
    Maybe one day I'll have more time than just being a human dependabot.

--- src/main/java/org/truje/jflix/integration/tmdb/model/MovieDiscoveryResponse.java
+++ src/main/java/org/truje/jflix/integration/tmdb/model/MovieDiscoveryResponse.java
@@ -8,27 +8,24 @@ import tools.jackson.databind.annotation.JsonNaming;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MovieDiscoveryResponse(
-	Integer page,
-	List<MovieDiscoveryResult> results,
-	Integer totalPages,
-	Integer totalResults) {
+        Integer page, List<MovieDiscoveryResult> results, Integer totalPages, Integer totalResults) {
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     public record MovieDiscoveryResult(
-	    Boolean adult,
-	    String backdropPath,
-	    List<Integer> genreIds,
-	    Long id,
-	    String title,
-	    String originalLanguage,
-	    String originalTitle,
-	    String overview,
-	    Double popularity,
-	    String posterPath,
-	    String releaseDate,
-	    Boolean softcore,
-	    Boolean video,
-	    Double voteAverage,
-	    Integer voteCount) {}
+            Boolean adult,
+            String backdropPath,
+            List<Integer> genreIds,
+            Long id,
+            String title,
+            String originalLanguage,
+            String originalTitle,
+            String overview,
+            Double popularity,
+            String posterPath,
+            String releaseDate,
+            Boolean softcore,
+            Boolean video,
+            Double voteAverage,
+            Integer voteCount) {}
 }
